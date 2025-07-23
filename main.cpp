61d30ea5d2602c85d1fd31b8d56d212c4dfb4d3b
David Fooks <david.fooks@larian.com>
2025-07-23 20:15:29 +0800

    Emscripten build runs without errors Added server.sh script

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
+#include <array>
+#include <chrono>
+#include <cstdio>
 #include <iostream>
 
-#ifndef EMSCRIPTEN
- #include <glad/gles2.h>
- #define GLFW_INCLUDE_NONE
+#ifdef EMSCRIPTEN
+	#include <emscripten/emscripten.h>
+	#include <emscripten/html5.h>
+	#include <GLES2/gl2.h>
+#else
+	#include <glad/gles2.h>
+	#define GLFW_INCLUDE_NONE
+	#include <GLFW/glfw3.h>
 #endif
-#include <GLFW/glfw3.h>
+namespace
+{
+	struct GLUserData
+	{
+	    GLuint m_Program;
+	    GLuint m_VBO;
+	    GLuint m_Texture;
 
+	    std::chrono::steady_clock::time_point m_LastFrameTime;
+	    double m_Time = 0.0;
+	};
+}
+
+void Update(GLUserData& /*glUserData*/)
+{
+	std::cout << "Hello world!\n";
+}
+
+#ifdef EMSCRIPTEN
+namespace
+{
+	void EmscriptenUpdate(void* userData)
+	{
+		GLUserData& glUserData = *static_cast<GLUserData*>(userData);
+		Update(glUserData);
+	}
+}
+#else
 namespace
 {
 	void error_callback(int /*error*/, const char* description)
@@ -18,39 +52,57 @@ namespace
 		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
-}
 
-bool InitWindow()
-{
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-	if (!window)
+	bool InitWindow(GLUserData& glUserData)
 	{
-		return false;
-	}
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-	glfwMakeContextCurrent(window);
-#ifndef EMSCRIPTEN
-	gladLoadGLES2(glfwGetProcAddress);
+		GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+		if (!window)
+		{
+			return false;
+		}
+
+		glfwMakeContextCurrent(window);
+		gladLoadGLES2(glfwGetProcAddress);
+		glfwSwapInterval(1);
+
+		glfwSetKeyCallback(window, key_callback);
+
+		while (!glfwWindowShouldClose(window))
+		{
+			Update(glUserData);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+
+		glfwDestroyWindow(window);
+		return true;
+	}
+}
 #endif
-	glfwSwapInterval(1);
-
-	glfwSetKeyCallback(window, key_callback);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-	return true;
-}
 
 int main()
 {
+    GLUserData glUserData;
+    glUserData.m_LastFrameTime = std::chrono::steady_clock::now();
+
+#ifdef EMSCRIPTEN
+    EmscriptenWebGLContextAttributes attr;
+    emscripten_webgl_init_context_attributes(&attr);
+    attr.alpha = 0;
+    attr.depth = 1;
+    attr.stencil = 0;
+    attr.antialias = 1;
+    attr.majorVersion = 2;
+    attr.minorVersion = 0;
+
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx = emscripten_webgl_create_context("#canvas", &attr);
+    emscripten_webgl_make_context_current(ctx);
+
+	emscripten_set_main_loop_arg(EmscriptenUpdate, static_cast<void*>(&glUserData), 0, 1);
+#else
 	if (!glfwInit())
 	{
 		std::cout << "Initialization failed\n";
@@ -58,9 +110,8 @@ int main()
 	}
 
 	glfwSetErrorCallback(error_callback);
-
-	InitWindow();
-
+	InitWindow(glUserData);
 	glfwTerminate();
+#endif
 	return 0;
 }
