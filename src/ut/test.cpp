d53096dea24b5321efe6930c088f4af0fef3257f
David Fooks <david.fooks@larian.com>
2025-07-29 21:48:53 +0800

    Split DemoProjects into DemoProjects and DemoProjectsLib so that tests can use the lib

--- src/ut/test.cpp
+++ src/ut/test.cpp
@@ -1,5 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Example test", "[example]") {
+#include <flecs/flecs.h>
+#include "Demo.h"
+
+TEST_CASE("Example test", "[example]")
+{
+    flecs::world world;
+    world.entity();
+
     REQUIRE(1 + 1 == 2);
 }
