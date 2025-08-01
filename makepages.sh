dab1c9e9cdc287d1dedfa999638ddc55fcc03ef5
David Fooks <david.fooks@larian.com>
2025-08-01 09:46:40 +0800

    Added built web project

---
+++ makepages.sh
@@ -0,0 +1,6 @@
+mkdir pages
+cp build_web/DemoProject.data pages
+cp build_web/DemoProject.html pages
+cp build_web/DemoProject.js pages
+cp build_web/DemoProject.wasm pages
+cp build_web/server.sh pages
---
+++ pages/server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+start http://127.0.0.1:8000/DemoProject.html
+python -m http.server 8000
